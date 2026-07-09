62883a1bd37871d1134facb9269bf14e9cccd6d5
Aaron Watters <awatters@flatironinstitute.org>
2026-07-09 11:33:39 -0400

    force inclusion of js file

--- setup.py
+++ setup.py
@@ -7,10 +7,11 @@ readme = open('README.md').read()
 setup(
     name="lineage_viewer",
     packages=["lineage_viewer"],
+    package_data={"lineage_viewer": ["lineage_doodle.js"]},
+    include_package_data=True,
     version=version,
     description="Graphical interface for editing a lineage and viewing related microscopy images.",
     long_description=readme,
-    include_package_data=True,
     author="Aaron Watters",
     author_email="awatters@flatironinstitute.org",
     url=url,
